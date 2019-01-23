050870bff593362d861fffc4551dc961e127ceab
originecode <42325924+originecode@users.noreply.github.com>
2019-01-23 13:29:08 +0100

    Sanitize Callback Function (checkbox)

--- inc/functions/customizer.php
+++ inc/functions/customizer.php
@@ -2,15 +2,20 @@
 
 function aemi_custom_settings( $wp_customize )
 {
+	function aemi_sanitize_checkbox( $checked )
+	{
+		return ( ( isset( $checked ) && true == $checked ) ? true : false );
+	}
 	
 	$wp_customize->add_section( 'aemi_features' , array(
 		'title'      => esc_html_x( 'AeMi - Features', 'aemi features section', 'aemi' ),
 		'priority'   => 0,
-		) );
+	) );
 
 	$wp_customize->add_setting( 'aemi_darkmode_display' , array(
 		'default'	=> true,
 		'transport'	=> 'refresh',
+		'sanitize_callback'	=> 'aemi_sanitize_checkbox'
 	) );
 	$wp_customize->add_control( 'aemi_darkmode_display', array(
 		'label' => esc_html_x( 'Dark Theme', 'darkmode activation' , 'aemi' ),
@@ -22,6 +27,7 @@ function aemi_custom_settings( $wp_customize )
 	$wp_customize->add_setting( 'aemi_search_button_display' , array(
 		'default'	=> true,
 		'transport'	=> 'refresh',
+		'sanitize_callback'	=> 'aemi_sanitize_checkbox'
 	) );
 	$wp_customize->add_control( 'aemi_search_button_display', array(
 		'label' => esc_html_x( 'Display Search Button in Top bar', 'search button display' , 'aemi' ),
@@ -29,7 +35,6 @@ function aemi_custom_settings( $wp_customize )
 		'settings' => 'aemi_search_button_display',
 		'type' => 'checkbox',
 	) );
-		
+
 }
 add_action( 'customize_register', 'aemi_custom_settings' );
-	
